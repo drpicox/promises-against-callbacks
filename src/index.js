1f058aa8b88fea7865cc927b511f365dcf9b0ee5
David Rodenas Pico <david.rodenas@gmail.com>
2016-02-01 17:00:41 +0100

    set master to initial state

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ var angular = require('angular');
 
 module.exports = angular.module('pacApp', [])
 
-.directive('pacBookDetails', require('./BookDetails.8.directive'))
-.directive('pacBookOverview', require('./BookOverview.8.directive'))
+.directive('pacBookDetails', require('./BookDetails.directive'))
+.directive('pacBookOverview', require('./BookOverview.directive'))
 
-.factory('httpBook', require('./HttpBook.8.factory'))
+.factory('httpBook', require('./HttpBook.factory'))
 
 // warmup
-.run(function(httpBook) { httpBook.get(); })
+//.run(function(httpBook) { httpBook.get(); })
 
 // simulate illegal op
 //.run(function(httpBook) { httpBook.get(function(book) {book.countPages();}); })
-.run(function(httpBook) { httpBook.get().then(function(book) { book.countPages(); }); })
+//.run(function(httpBook) { httpBook.get().then(function(book) { book.countPages(); }); })
 
 .name;
